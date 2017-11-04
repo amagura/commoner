8f9d398f13b2f192c22cfe5a8f873a328b384e93
amagura <sickhadas.code@gmail.com>
2017-11-03 19:31:11 -0600

    updates some code

--- lisp/stdio.lisp
+++ lisp/stdio.lisp
@@ -14,37 +14,86 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 |#
-(declaim (inline argc*))
+(load #P "./unstd.lisp")
+(load #P "./stdlib.lisp"))
 
-(defun argc* ()
-  "returns the number of arguments available to the current program"
-  (or #+clisp         (length *args*)
-      #+sbcl          (length *posix-argv*)
-      #+abcl          (length *command-line-argument-list*)
-      #+(or mkcl ecl) (si:argc)
-      nil))
+(defvar *argc* nil)
+(defvar *progname* nil)
+
+(defmacro .mkargs. (&key argc not-implemented)
+  ;; generate either *argc* or `(or)' for argv*
+  (let ((ni (gensym "n-i")))
+    `(let* ((,ni ,not-implemented)
+            (%argv% (or #+abcl              ext:*command-line-argument-list*
+                        #+allegro           (sys:command-line-arguments)
+                        #+(or clasp ecl)    (loop for i from 0 below (si:argc)
+                                                  collect (si:argv i))
+                        #+clisp             *args*
+                        #+clozure           ccl:*command-line-argument-list*
+                        #+(or cmucl scl)    extensions:*command-line-strings*
+                        #+gcl               si:*command-args*
+                        #+(or genera mcl)   nil
+                        #+lispworks         sys:*line-arguments-list*
+                        #+mkcl              (loop for i from 0 below (mkcl:argc)
+                                                  collect (mkcl:argv i))
+                        #+sbcl              sb-ext:*posix-argv*
+                        #+xcl               system:*argv*
+                        #-(or abcl allegro clasp clisp clozure cmucl ecl gcl
+                              genera lispworks mcl mkcl sbcl scl xcl)
+                        (error 'not-implemented :proc (or ,not-implemented 'argv*)))))
+       ,(if (not (null argc))
+            `(setf *argc* (length %argv%))
+            `%argv%))))
+
+;; set *argc* according to current implementation of CL
+(.mkargs. :argc t :not-implemented '*argc*)
 
 (defun argv* (&optional .nth.)
   "returns either a list of args or a particular arg if .nth. is not null.
    calls `error' if something goes wrong."
-  (if (null .nth.)
-      (or #+clisp *args*
-          #+sbcl  *posix-argv*
-          #+abcl  *command-line-argument-list*
-          nil)
-      (if (< .nth. (argc*)) ; make sure that we have enough args to get .nth. arg
-          (or #+clisp (nth .nth. *args*)
-              #+sbcl  (nth .nth. *posix-argv*)
-              #+abcl  (nth .nth. *command-line-argument-list*)
-              nil)
-          (error "error: position ~A is out of bounds: list has ~A ~A."
-                 (+ .nth. 1)
-                 (argc*)
-                 (if (> (argc*) 1) ; use position, if there is only 1, or positions.
-                     "indices"
-                     (if (< (argc*) 1)
-                         "indices"
-                         "index"))))))
+  (let ((%argv% (.mkargs.)))
+    (if (null .nth.)
+        (if (< .nth. *argc*) ; make sure that we have enough args to get .nth. arg
+            (nth .nth. %argv%))
+        (error "argv*: position ~A is out of bounds: list has ~A ~A."
+               (+ .nth. 1)
+               *argc*
+               (if (> *argc* 1) ; use position, if there is only 1, or positions.
+                   "indices"
+                   (if (< *argc* 1)
+                       "indices"
+                       "index"))))))
 
-;; cease treating argc* like an inline function, so that other functions will call it normally
-(declaim (notinline argc*))
+;(defun progname (&optional basename)
+  ;(or #+abcl            nil
+      ;#+allegro         nil
+      ;#+(or clasp ecl)  (si:argv 0)
+
+;;; Standard Streams
+(defvar *stdin* *standard-input*)
+(defvar *stdout* *standard-output*)
+(defvar *stderr* *error-output*)
+
+(setf *stdin*
+      #.(or #+clozure         'ccl::*stdin*
+            #+(or cmucl scl)  'system:*stdin*
+            #+(or clasp ecl)  'ext:+process-standard-input+
+            #+sbcl            'sb-sys:*stdin*
+            '*stdin*))
+
+(setf *stdout*
+      #.(or #+clozure         'ccl::*stdout*
+            #+(or cmucl scl)  'system:*stdout*
+            #+(or clasp ecl)  'ext:+process-standard-output+
+            #+sbcl            'sb-sys:*stdout*
+            '*stdout*))
+
+(setf *stderr*
+      #.(or #+allegro         'excl::*stderr*
+            #+clozure         'ccl::*stderr*
+            #+(or cmucl scl)  'system:*stderr*
+            #+(or clasp ecl)  'ext:+process-error-output+
+            #+sbcl            'sb-sys:*stderr*
+            '*stderr*))
+
+;;; Safe Syntax
--- lisp/stdlib.lisp
+++ lisp/stdlib.lisp
@@ -1,6 +1,107 @@
-(defun odd (list)
-  "returns the items appearing at odd indices within a list"
-  (do ((l list (cddr l)))
-      (x '() (cons (car l) x)))
-  ((null l)
-   (nreverse x)))
+;(defun odd (list)
+  ;"returns the items appearing at odd indices within a list"
+  ;(do ((l list (cddr l))
+      ;(x '() (cons (car l) x)))
+  ;((null l)
+   ;(nreverse x))))
+
+;; Die
+;; (die (exit format &rest args))
+;(defun die (exit format &rest args)
+  ;"Die in error with custom error message"
+  ;(with-safe-io-syntax )
+
+(defun getenv* (name &optional default)
+  "get enviroment variables"
+  #+cmu
+  (let ((x (assoc name ext:*environment-list*
+                  :test #'string=)))
+    (if x (cdr x) default))
+  #-cmu
+  (or
+    #+allegro   (sys:getenv name)
+    #+clisp     (ext:getenv name)
+    #+ecl       (si:getenv name)
+    #+sbcl      (sb-unix::posix-getenv name)
+    #+lispworks (lispworks:environment-variable name)
+    default))
+
+(defun exit* (&optional code)
+  (or
+    ;; This group from "clocc-port/ext.lisp"
+    #+clisp             (#+lisp=cl ext:quit #-lisp=cl lisp:quit code)
+    #+sbcl              (sb-ext:exit :code code)
+    #+allegro           (excl:exit code)
+    #+cormanlisp        (win32:exitprocess code)
+    #+gcl               (lisp:bye code)           ; XXX or is it LISP::QUIT?
+    #+lispworks         (lw:quit :status code)
+    #+lucid             (lcl:quit code)
+    ;; This group from Maxima
+    #+kcl               (lisp::bye)               ; XXX Does this take an arg?
+    #+scl               (ext:quit code)           ; XXX Pretty sure this *does* take an arg.
+    #+(or openmcl mcl)  (ccl::quit)
+    #+abcl              (cl-user::quit)
+    #+ecl               (si:quit)
+    ;; This group from <hebi...@math.uni.wroc.pl>
+    #+poplog            (poplog::bye)             ; XXX does this take an arg?
+    #-(or allegro clisp
+          cmu cormalisp
+          gcl lispworks
+          lucid sbcl
+          kcl scl
+          openmcl mcl
+          abcl ecl)
+    (not-implemented-error 'exit*)))
+
+(defun unquote (quoted-sexp)
+  ;; this takes advantage of the fact that
+  ;; function arguments are evaluated before
+  ;; being processed
+  ;; XXX this is basically just a trick to evaluate
+  ;; some code once (in case it is quoted, but we want it to act like it isn't)
+  quoted-sexp)
+
+#|/////////////////////////////////|#
+;;;; Additional Flow Control
+#|/////////////////////////////////|#
+(defmacro nest (&rest .rest.) ;; taken from uiop/utility.lisp
+  "macro that keeps indentation under control"
+  (reduce #'(lambda (outer inner) `(,@outer ,inner))
+          .rest. :from-end t))
+
+(defmacro if-let (bindings &body (then-form &optional else-form)) ;; from uiop/utility.lisp
+  "bindings can be (var form) or ((var1 form1) ...)"
+  (let* ((binding-list (if (and (consp bindings) (symbolp (car bindings)))
+                           (list bindings)
+                           bindings))
+         (variables (mapcar #'car binding-list)))
+    `(let ,binding-list
+       (if (and ,@variables)
+           ,then-form
+           ,else-form))))
+
+(defun length= (text expected-length)
+  ; test if (= (length text) expected-length) is true
+  (= (length text) expected-length))
+
+(defun if-length (cond then-form &optional else-form)
+  (if (length= (car cond) (cadr cond))
+      then-form
+      (if (not (null else-form))
+          else-form)))
+
+(defun not-nullp (sexp)
+  ;; A very basic NOT NULL
+  (not (null sexp)))
+
+(defmacro undefinedp (symbol)
+  ;; returns T if the symbol is not defined
+  ;; and nil if it is defined
+  (let ((%z% (gensym "masked-symbol")))
+    `(handler-case (and (let ((,%z% ,symbol)) ,%z%) nil) ((or undefined-function unbound-variable) () t))))
+
+(defmacro definedp (symbol)
+  ;; trivial macro that returns nil if a variable is not defined
+  ;; and t if it is defined
+  (let ((%z% (gensym "masked-symbol")))
+    `(handler-case (and (let ((,%z% ,symbol)) ,%z%) t) ((or undefined-function unbound-variable) () nil))))
