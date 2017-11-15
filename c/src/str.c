1669950de42c68d323428ca60924fdf7d4e714d4
Alexej Magura <sickhadas.code@gmail.com>
2017-11-15 13:21:24 -0700

    updates str.c to use strdup for const char

--- c/src/str.c
+++ c/src/str.c
@@ -1,20 +1,20 @@
 /****
-COMMON; str.c, String related functions
+  COMMON; str.c, String related functions
 
-Copyright (C) 2015, 2016, 2017 Alexej G. Magura
+  Copyright (C) 2015, 2016, 2017 Alexej G. Magura
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ****/
 #ifndef COMMON_STRING_C_GUARD
 # define COMMON_STRING_C_GUARD 1
@@ -34,30 +34,40 @@ int *strndelim(const char *s, const char od, const char cd, int count[2])
 # if defined(_GNU_SOURCE)
      char *c = strchr(s, '\0');
 # else
-     char *c = &s[strlen(s)];
+     char *tmp = strdup(s);
+     char *c = &tmp[strlen(tmp)];
 # endif
      if (c == s)
-	  return NULL;
+          goto fail;
 
      do {
-	  if (c != s && cpeek(c, s, 0) == '\\')
-	       continue;
-	  if (*c == cd)
-	       ++count[1];
-	  else if (*c == od)
-	       ++count[0];
+          if (c != s && cpeek(c, s, 0) == '\\')
+               continue;
+          if (*c == cd)
+               ++count[1];
+          else if (*c == od)
+               ++count[0];
      } while (c-- != s);
 
      if (od == cd && count[1] > 0) {
-	  if (count[1] % 2 == 1)
-	       while (count[0]++ < --count[1]);
-	  else {
-	       count[0] = count[1] * 0.5;
-	       count[1] *= 0.5;
-	  }
+          if (count[1] % 2 == 1)
+               while (count[0]++ < --count[1]);
+          else {
+               count[0] = count[1] * 0.5;
+               count[1] *= 0.5;
+          }
      }
+# if !defined(_GNU_SOURCE)
+     free(tmp);
+# endif
 
      return count;
+fail:
+# if !defined(_GNU_SOURCE)
+     free(tmp);
+# endif
+     return NULL;
+
 }
 
 char *strwodqp(const char *src)
@@ -68,10 +78,10 @@ char *strwodqp(const char *src)
      tmp = token = rest = newp = NULL;
 
      if (!strndelim(src, '"', '"', c))
-	  return NULL;
+          return NULL;
 
      if (c[0] == 0)
-	  return NULL;
+          return NULL;
 
      tmp = strdup(src);
      newp = malloc(n);
@@ -80,18 +90,18 @@ char *strwodqp(const char *src)
      token = strtok_r(tmp, "\"", &rest);
 
      if (token == NULL) {
-	  free(newp);
-	  return NULL;
+          free(newp);
+          return NULL;
      }
 
      catl(newp, n, token);
      while ((token = strtok_r(NULL, "\"", &rest)) != NULL) {
-	  if (even % 2 == 1) {
-	       catm(newp, n, newp, token);
-	       --even;
-	  } else {
-	       ++even;
-	  }
+          if (even % 2 == 1) {
+               catm(newp, n, newp, token);
+               --even;
+          } else {
+               ++even;
+          }
      }
 
      free(tmp);
@@ -109,13 +119,13 @@ int strwodq(char *dst, const char *src, size_t n)
      char *newp = NULL;
 
      if (!strndelim(src, '"', '"', c)) {
-	  r += 1;
-	  goto end;
+          r += 1;
+          goto end;
      }
 
      if (c[0] == 0) {
-	  r += 2;
-	  goto end;
+          r += 2;
+          goto end;
      }
 
      tmp = strdup(src);
@@ -124,18 +134,18 @@ int strwodq(char *dst, const char *src, size_t n)
 
      token = strtok_r(tmp, "\"", &rest);
      if (token == NULL) {
-	  r += 3;
-	  goto free;
+          r += 3;
+          goto free;
      }
      catl(newp, n, token);
 
      while ((token = strtok_r(NULL, "\"", &rest)) != NULL) {
-	  if (even % 2 == 1) {
-	       catm(newp, n, newp, token);
-	       --even;
-	  } else {
-	       ++even;
-	  }
+          if (even % 2 == 1) {
+               catm(newp, n, newp, token);
+               --even;
+          } else {
+               ++even;
+          }
      }
 
      memcpy(dst, newp, n);
